73133652aa4f1fb55195e897cec2a5d0761ef504
Karol Michalski <karol.michalski95@gmail.com>
2018-10-14 22:32:24 +0200

    implement MyAppGlideModule

--- app/src/main/java/pl/karolmichalski/githubrepos/presentation/utils/BindingAdapters.kt
+++ app/src/main/java/pl/karolmichalski/githubrepos/presentation/utils/BindingAdapters.kt
@@ -2,20 +2,16 @@ package pl.karolmichalski.githubrepos.presentation.utils
 
 import android.widget.ImageView
 import androidx.databinding.BindingAdapter
-import com.bumptech.glide.Glide
-import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
-import com.bumptech.glide.request.RequestOptions
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions.withCrossFade
 import pl.karolmichalski.githubrepos.R
+import pl.karolmichalski.githubrepos.data.glide.GlideApp
+
 
 @BindingAdapter("imageUrl")
 fun ImageView.setImageUrl(url: String?) {
-	val options = RequestOptions()
-			.placeholder(R.drawable.placeholder_avatar)
-	val transition = DrawableTransitionOptions()
-			.crossFade()
-	Glide.with(this)
+	GlideApp.with(this)
 			.load(url)
-			.apply(options)
-			.transition(transition)
+			.placeholder(R.drawable.placeholder_avatar)
+			.transition(withCrossFade())
 			.into(this)
 }
