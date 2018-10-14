795c867da4d4ed0c6ceec7357a92744f35750f51
Karol Michalski <karol.michalski95@gmail.com>
2018-10-14 16:12:50 +0200

    User: add id field

--- app/src/main/java/pl/karolmichalski/githubrepos/data/models/User.kt
+++ app/src/main/java/pl/karolmichalski/githubrepos/data/models/User.kt
@@ -2,6 +2,7 @@ package pl.karolmichalski.githubrepos.data.models
 
 import com.fasterxml.jackson.annotation.JsonProperty
 
-class User(@JsonProperty("login") val login: String,
+class User(@JsonProperty("id") val id: Int,
+		   @JsonProperty("login") val login: String,
 		   @JsonProperty("avatar_url") val avatarUrl: String,
 		   @JsonProperty("repos_url") val reposUrl: String)
